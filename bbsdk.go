be99a264e03d12f9a740344800377af314f18aeb
wfunc <114468522+wfunc@users.noreply.github.com>
2025-05-13 14:40:23 +0800

    first commit

---
+++ bbsdk.go
@@ -0,0 +1,126 @@
+package bbsdk
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/wfunc/util/xhash"
+	"github.com/wfunc/util/xmap"
+)
+
+const (
+	KeyXBBID    = "X-BB-Id"
+	KeyXBBTOKEN = "X-BB-Token"
+
+	CreateUserURL    = "/api/v2/createUser"
+	LoginURL         = "/api/v2/login"
+	TransferURL      = "/api/v2/transfer"
+	GetGameRecordURL = "/api/v2/getGameRecord"
+	GetBalanceURL    = "/api/v2/getBalance"
+	password         = "123"
+)
+
+var (
+	URL       string
+	BBID      string
+	BBTOKEN   string
+	Uppername string
+	Client    *http.Client
+)
+
+func Bootstrap(url, bbid, bbtoken, uppername string) {
+	URL = url
+	BBID = bbid
+	BBTOKEN = bbtoken
+	Uppername = uppername
+	Client = &http.Client{
+		Timeout: 5 * time.Second,
+	}
+}
+
+//	type CreateUserParams struct {
+//		Username string `form:"Username" binding:"required"`
+//		Password string `form:"Password" binding:"required"`
+//	}
+func CreateUser(username string) (xmap.M, error) {
+	url := fmt.Sprintf("%s%s?Username=%s&Password=%s", URL, CreateUserURL, username, password)
+	return GET(url)
+}
+
+func Transfer(username, remitNo, action string, remit string) (xmap.M, error) {
+	url := fmt.Sprintf("%s%s?Username=%s&RemitNo=%s&Action=%s&Remit=%s", URL, TransferURL, username, remitNo, action, remit)
+	return GET(url)
+}
+
+func GetGameRecord(action, date, starttime, endtime, gametype, page, pagelimit string) (xmap.M, error) {
+	params := fmt.Sprintf("action=%s&date=%s&starttime=%s&endtime=%s", action, date, starttime, endtime)
+	// &gametype=%s&page=%s&pagelimit=%s
+	if len(gametype) > 0 {
+		params += fmt.Sprintf("&gametype=%s", gametype)
+	}
+	if len(page) > 0 {
+		params += fmt.Sprintf("&page=%s", page)
+	}
+	if len(pagelimit) > 0 {
+		params += fmt.Sprintf("&pagelimit=%s", pagelimit)
+	}
+	url := fmt.Sprintf("%s%s?%s", URL, GetGameRecordURL, params)
+	return GET(url)
+}
+
+func AdjustDateByTimezone() string {
+	loc, _ := time.LoadLocation("Asia/Shanghai")
+	currentTime := time.Now().In(loc)
+
+	_, offset := currentTime.Zone()
+	if offset == 8*3600 {
+		// 当前是东八区，减去 12 小时
+		adjustedTime := currentTime.Add(-8 * time.Hour)
+		return adjustedTime.Format("2006-01-02")
+	}
+
+	return currentTime.Format("2006-01-02")
+}
+
+func Login(username string) string {
+	date := AdjustDateByTimezone()
+	before := date + "Uppername:" + Uppername + "Username:" + username + BBTOKEN
+	key := xhash.SHA256([]byte(before))
+	url := fmt.Sprintf("%s%s?Username=%s&Uppername=%s&Key=%s", URL, LoginURL, username, Uppername, key)
+	return url
+}
+
+func GET(url string) (xmap.M, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set(KeyXBBID, BBID)
+	req.Header.Set(KeyXBBTOKEN, BBTOKEN)
+
+	resp, err := Client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("request failed: %s", resp.Status)
+	}
+
+	var result xmap.M
+	if err := json.Unmarshal(respBody, &result); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/wfunc/bbsdk
+
+go 1.24.1
+
+require github.com/wfunc/util v0.0.0-20241214111718-546afdabf3c8
+
+require golang.org/x/net v0.31.0 // indirect
